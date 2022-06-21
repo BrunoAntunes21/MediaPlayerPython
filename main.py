e1579cf27cf4e8b3325768a0b8a73a5b8aa55a10
bruno antunes <68615491+BrunoAntunes21@users.noreply.github.com>
2022-06-21 19:35:55 -0300

    configuração dos atributos conte erro não está renderizando a tela da maneira correta

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #criação da janela
-from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QHBoxLayout,QVBoxLayout
 #configuração dos atributos da janela
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon,QPalette
+from PyQt5.QtMultimedia import QMediaPlayer,QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import Qt
 import sys
 class Window(QWidget):
     def __init__(self):
@@ -13,9 +16,41 @@ class Window(QWidget):
         #configuração do eixo x e y(posicionamento da tela)
         self.setGeometry(350,100,700,500)
 
+        # configuração da paletta de cores
+        p = self.palette()
+        # configuração da paleta de cores da janela
+        p.setColor(QPalette.Window, Qt.GlobalColor.red)
+
+        self.create_player()
+
+    #criação do player
+    def create_player(self):
+        self.mediaPlayer=QMediaPlayer(None,QMediaPlayer.VideoSurface)
+        videowidget= QVideoWidget()
+
+        self.openBtn=QPushButton()
+
+        self.openBtn=QPushButton('Open video')
+        #configuração da posição horizontal da tela
+        hbox=QHBoxLayout()
+        hbox.setContentsMargins(0,0,0,0)
+        hbox.addWidget(self.openBtn)
+
+        #configuração da posição vertical da tela
+        vbox=QVBoxLayout()
+        vbox.setContentsMargins(0,0,0,0)
+
+        self.setLayout(vbox)
+
+
+
+
 #criação da aplicação e da janela
 app=QApplication(sys.argv)
 #janela
 window=Window()
 window.show()
 sys.exit(app.exec_())
+
+
+
