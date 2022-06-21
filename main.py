e1e2eb13c1cd9c7ad533128cd254cc21aacb8c5b
bruno antunes <68615491+BrunoAntunes21@users.noreply.github.com>
2022-06-21 18:41:08 -0300

    configuração da tela,mudança do icone e noenclatura do projeto adicionaddos

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+#criação da janela
 from PyQt5.QtWidgets import QApplication,QWidget
+#configuração dos atributos da janela
+from PyQt5.QtGui import QIcon
 import sys
 class Window(QWidget):
     def __init__(self):
         super().__init__()
+        #configuração de um icone e configuração do mesmo
+        self.setWindowIcon(QIcon("botoes.ico"))
+        #configuração do texto da janela
+        self.setWindowTitle("prototipo de aplicativo de multimidia feito em Python")
+        #configuração do eixo x e y(posicionamento da tela)
+        self.setGeometry(350,100,700,500)
 
 #criação da aplicação e da janela
 app=QApplication(sys.argv)
